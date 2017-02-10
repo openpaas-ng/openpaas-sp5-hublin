1af7656abf4939fcdabf22d63124513808196374
samy <samy.benhamiche@gmail.com>
2017-02-10 09:46:04 +0100

    Store summary and keywords into OpenPaas Storage
    Add function `storeIntoOpenP` to call storage REST API
    Add new config for OpenPaaS REST path
    Add node-rest-client dependency

---
+++ backend/webserver/saveTranscript.js
@@ -0,0 +1,19 @@
+var OpenPaasStore = require('../../config/default.json').OpenPaasStore;
+var Client = require('node-rest-client').Client;
+
+var client = new Client();
+
+// Store the result of the summary module into OpenPaaS Storage
+exports.storeIntoOpenP = function (trans_data) {
+		var args = {
+				path: {},
+				parameters: {},
+				headers: { "Content-Type": "application/json" },
+				data: trans_data
+		};
+		client.registerMethod("sendToOpenP", OpenPaasStore + "/summary", "POST");
+
+		client.methods.sendToOpenP(args, function (data, response) {
+			// TODO chack response code for error
+		});
+}
